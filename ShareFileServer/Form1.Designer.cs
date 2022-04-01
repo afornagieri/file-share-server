e60f13eb6e287d69f1aa9740f106c25f5ff70fe1
afornagieri <fornagieri.artur@gmail.com>
2022-03-31 22:37:43 -0300

    Ready to receive files

---
+++ ShareFileServer/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace ShareFileServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.NumericUpDown();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnStopServer = new System.Windows.Forms.Button();
+            this.openFileSearchFolder = new System.Windows.Forms.LinkLabel();
+            this.listLogs = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(776, 62);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "File Share Server";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(18, 103);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(354, 31);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(378, 103);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(94, 31);
+            this.txtPort.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(18, 169);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(382, 44);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnStopServer
+            // 
+            this.btnStopServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStopServer.Location = new System.Drawing.Point(406, 169);
+            this.btnStopServer.Name = "btnStopServer";
+            this.btnStopServer.Size = new System.Drawing.Size(382, 44);
+            this.btnStopServer.TabIndex = 4;
+            this.btnStopServer.Text = "STOP SERVER";
+            this.btnStopServer.UseVisualStyleBackColor = true;
+            this.btnStopServer.Click += new System.EventHandler(this.btnStopServer_Click);
+            // 
+            // openFileSearchFolder
+            // 
+            this.openFileSearchFolder.AutoSize = true;
+            this.openFileSearchFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openFileSearchFolder.Location = new System.Drawing.Point(18, 236);
+            this.openFileSearchFolder.Name = "openFileSearchFolder";
+            this.openFileSearchFolder.Size = new System.Drawing.Size(246, 25);
+            this.openFileSearchFolder.TabIndex = 5;
+            this.openFileSearchFolder.TabStop = true;
+            this.openFileSearchFolder.Text = "Click here to open folder";
+            this.openFileSearchFolder.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.openFileSearchFolder_LinkClicked);
+            // 
+            // listLogs
+            // 
+            this.listLogs.FormattingEnabled = true;
+            this.listLogs.Location = new System.Drawing.Point(18, 295);
+            this.listLogs.Name = "listLogs";
+            this.listLogs.Size = new System.Drawing.Size(770, 147);
+            this.listLogs.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listLogs);
+            this.Controls.Add(this.openFileSearchFolder);
+            this.Controls.Add(this.btnStopServer);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.NumericUpDown txtPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnStopServer;
+        private System.Windows.Forms.LinkLabel openFileSearchFolder;
+        private System.Windows.Forms.ListBox listLogs;
+    }
+}
+
---
+++ ShareFileServer/SFServer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShareFileServer
+{
+    class SFServer
+    {
+        static IPEndPoint endpoint;
+        static Socket server;
+        
+        public static string localhost = "127.0.0.1";
+        public static int port = 1000;
+        public static string destinationFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\";
+        public static ListBox listOfMessages;
+
+        public static void Start()
+        {
+            try
+            {
+                endpoint = new IPEndPoint(IPAddress.Parse(localhost), port);
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                server.Bind(endpoint);
+            }
+            catch (Exception ex)
+            {
+                listOfMessages.Invoke(new Action(() => listOfMessages.Items.Add("Error while trying to initiate the server: \n" + ex)));
+                listOfMessages.SetSelected(listOfMessages.Items.Count - 1, true);
+                return;
+            }
+
+            try
+            {
+                server.Listen(100);
+
+                listOfMessages.Invoke(new Action(() => listOfMessages.Items.Add("Server ready to receive files !")));
+                listOfMessages.SetSelected(listOfMessages.Items.Count - 1, true);
+
+                Socket client = server.Accept();
+                client.ReceiveBufferSize = 16384;
+
+                byte[] data = new byte[1024 * 50000];
+                int lengthOfReceivedBytes = client.Receive(data, data.Length, 0);
+                int fileNameLength = BitConverter.ToInt32(data, 0);
+                string fileName = Encoding.UTF8.GetString(data, 4, fileNameLength);
+
+                BinaryWriter writer = new BinaryWriter(File.Open(destinationFolder + fileName, FileMode.Append));
+                writer.Write(data, 4 + fileNameLength, lengthOfReceivedBytes - 4 - fileNameLength);
+
+                while (lengthOfReceivedBytes > 0)
+                {
+                    lengthOfReceivedBytes = client.Receive(data, data.Length, 0);
+
+                    if(lengthOfReceivedBytes == 0)
+                    {
+                        writer.Close();
+                    }
+                    else
+                    {
+                        writer.Write(data, 0, lengthOfReceivedBytes);
+                    }
+
+                    listOfMessages.Invoke(new Action(() => listOfMessages.Items.Add($"File received {fileName}\n")));
+                    listOfMessages.SetSelected(listOfMessages.Items.Count - 1, true);
+
+                    writer.Close();
+                    client.Close();
+                }
+
+            }
+            catch (SocketException ex)
+            {
+                listOfMessages.Invoke(new Action(() => listOfMessages.Items.Add($"Error while receiving a file: " + ex)));
+                listOfMessages.SetSelected(listOfMessages.Items.Count - 1, true);
+            }
+            finally
+            {
+                server.Close();
+                server.Dispose();
+                Start();
+            }
+        }
+
+    }
+}
